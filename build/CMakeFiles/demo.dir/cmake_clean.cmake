2705708f4e20ccd0b3ab2a866d87660858d203e1
shenglish <2285575104@qq.com>
2021-03-14 13:19:26 +0800

    first use cmake

---
+++ build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/vscode_test/main.cpp.o"
+  "CMakeFiles/demo.dir/vscode_test/test_class.cpp.o"
+  "demo.pdb"
+  "demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
