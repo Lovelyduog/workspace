40bccf1ec7959373061da90dd143af89c393e36c
shenglish <2285575104@qq.com>
2021-03-14 22:28:09 +0800

    add template dir

--- cpp_practice/main.cpp
+++ cpp_practice/main.cpp
@@ -1,13 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-03-13 23:54:16
- * @LastEditTime: 2021-03-14 22:05:52
+ * @LastEditTime: 2021-03-14 22:27:02
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vscode_test/main.cpp
  */
 #include <iostream>
 #include <vector>
+#include "template_practice/template.h"
 int fun()
 {
     int a;
