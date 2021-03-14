ad98ca75e9a828375478d1dcef1aefd78596b802
shenglish <2285575104@qq.com>
2021-03-14 12:14:05 +0800

    test 2

--- vscode_test/test_class.h
+++ vscode_test/test_class.h
@@ -1,8 +1,17 @@
-
+/*
+ctrl+alt+i自动生成注释
+ * @Author: your name
+ * @Date: 2021-03-14 00:14:07
+ * @LastEditTime: 2021-03-14 12:13:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vscode_test/test_class.h
+ */
 class TestClass
 {
 public: 
     void fun();
 private:
     int a;
+    int b;
 };
