7465e82dbe4487cc422a95e88e5bb1aeb79575bd
shenglish <2285575104@qq.com>
2021-03-14 13:44:52 +0800

    test over

--- vscode_test/main.cpp
+++ vscode_test/main.cpp
@@ -1,9 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-13 23:54:16
+ * @LastEditTime: 2021-03-14 13:43:21
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /vscode_test/main.cpp
+ */
 #include <iostream>
 #include "test_class.h"
-
+#include <vector>
+int fun()
+{
+    int a;
+    int c;
+    std::vector<int> vec;
+    for(int i = 0; i < 10 ; ++i)
+    {
+        vec.push_back(i);
+    }
+}
 int main()
 {
     TestClass a;
+    fun();
     std::cout << "hh" << std::endl;
     return 0;
 }
